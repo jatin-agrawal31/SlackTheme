9f1af3ce567f63592675ff250824459cd7ef7abc
Eric Harvey <eharvey2@gmail.com>
2018-09-08 12:56:49 -0700

    whoops

--- eric-urls.js
+++ eric-urls.js
@@ -3,6 +3,6 @@
   'https://raw.githubusercontent.com/EricHarvey/SlackTheme/master/hljs-base-dracula.css',
   'https://raw.githubusercontent.com/EricHarvey/SlackTheme/master/base-eric.css',
   'https://raw.githubusercontent.com/EricHarvey/SlackTheme/master/hljs-brogrammer.css'
-  ];,
+  ],
   customJsUrl: 'https://raw.githubusercontent.com/EricHarvey/SlackTheme/master/eric-custom-js.js'
 }})();
