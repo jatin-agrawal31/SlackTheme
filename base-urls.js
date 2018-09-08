d0dd9af5330e1fd0d39fb1b04b7431a38fdd17de
Eric Harvey <eharvey2@gmail.com>
2018-09-08 12:40:03 -0700

    WIP

---
+++ base-urls.js
@@ -0,0 +1,5 @@
+const cssUrls = ['https://raw.githubusercontent.com/EricHarvey/SlackTheme/master/base-dark.css',
+                  'https://raw.githubusercontent.com/EricHarvey/SlackTheme/master/hljs-base-dracula.css',
+                  ];
+
+const customJsUrl = 'https://raw.githubusercontent.com/EricHarvey/SlackTheme/master/base-custom-js.js';
