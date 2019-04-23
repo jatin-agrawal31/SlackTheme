3364116f1b67fc31ba494d0f150a531afdd353be
Eric Harvey <eharvey2@gmail.com>
2019-04-23 15:20:51 -0700

    Test stuff

--- eric-custom-js.js
+++ eric-custom-js.js
@@ -11,7 +11,7 @@ $('a[data-message-sender="U0XK8MGFP"]:not(.name-changed)').each(function(i, bloc
   block.classList.add('name-changed');
 });
 $('a[data-message-sender="U1LDS3P4H"]:not(.name-changed)').each(function(i, block) {
-  block.innerText = "<3 Sandip";
+  block.innerText = "\"Coolest guy in Informed\"";
   block.classList.add('name-changed');
 });
 $('a[data-message-sender="UB1BEA42X"]:not(.name-changed)').each(function(i, block) {
@@ -22,14 +22,31 @@ $('a[data-message-sender="U3M8891V5"]:not(.name-changed)').each(function(i, bloc
   block.innerText = "Running Late 🏃";
   block.classList.add('name-changed');
 });
+
 $('a[data-message-sender="UDSAFEE2D"]:not(.name-changed)').each(function(i, block) {
-  block.innerText = "Victoria Duong";
+  block.innerText = "Definitely has too much power and must be stopped 👸";
   block.classList.add('name-changed');
 });
 $('a[data-message-sender="UEEMUVD5M"]:not(.name-changed)').each(function(i, block) {
-  block.innerText = "Jatin Agrawal";
+  block.innerText = "Ruff Warrior";
   block.classList.add('name-changed');
 });
+$('a[data-message-sender="UFCN2ETFB"]:not(.name-changed)').each(function(i, block) {
+  block.innerText = "Mad Swami";
+  block.classList.add('name-changed');
+});
+$('a[data-message-sender="UFX3LPZS9"]:not(.name-changed)').each(function(i, block) {
+  block.innerText = "Excelsior";
+  block.classList.add('name-changed');
+});
+$('a[data-message-sender="UGB100S8L"]:not(.name-changed)').each(function(i, block) {
+  block.innerText = "诸海婷";
+  block.classList.add('name-changed');
+});
+// $('span:contains(":wappah"):not(.name-changed)'.each(function(i, block) {
+//   block.display = 'none';
+//   block.classList.add('name-changed');
+// });
 if ($('link#inserted-fonts').length === 0) {
   const html = '<link rel="stylesheet" href=https://fonts.googleapis.com/css?family=Asap|Roboto:100,300|Ubuntu:300,400" id="inserted-fonts"/>'
   $(html).appendTo('head');
