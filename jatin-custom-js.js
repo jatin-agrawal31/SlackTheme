1708c8e1d254026b12e42b500be000242fff3a1d
Jatin <jatin.agrawal@driveinformed.com>
2019-04-30 17:10:31 -0700

    added files back

---
+++ jatin-custom-js.js
@@ -0,0 +1,36 @@
+$('a[data-message-sender="U7UKE4JN4"]:not(.name-changed)').each(function(i, block) {
+  block.innerText = 'Wei Wei';
+  block.classList.add('name-changed');
+});
+$('a[data-message-sender="UAU3VA7JB"]:not(.name-changed)').each(function(i, block) {
+  block.innerText = 'Proposition "Ashley" Chicken 🐔';
+  block.classList.add('name-changed');
+});
+$('a[data-message-sender="U0XK8MGFP"]:not(.name-changed)').each(function(i, block) {
+  block.innerText = "I'm CEO,";
+  block.classList.add('name-changed');
+});
+$('a[data-message-sender="U1LDS3P4H"]:not(.name-changed)').each(function(i, block) {
+  block.innerText = "<3 Sandip";
+  block.classList.add('name-changed');
+});
+$('a[data-message-sender="UB1BEA42X"]:not(.name-changed)').each(function(i, block) {
+  block.innerText = "Utility Bill";
+  block.classList.add('name-changed');
+});
+$('a[data-message-sender="U3M8891V5"]:not(.name-changed)').each(function(i, block) {
+  block.innerText = "Running Late 🏃";
+  block.classList.add('name-changed');
+});
+$('a[data-message-sender="UDSAFEE2D"]:not(.name-changed)').each(function(i, block) {
+  block.innerText = "Victoria Duong";
+  block.classList.add('name-changed');
+});
+$('a[data-message-sender="UEEMUVD5M"]:not(.name-changed)').each(function(i, block) {
+  block.innerText = "Jatin Agrawal";
+  block.classList.add('name-changed');
+});
+if ($('link#inserted-fonts').length === 0) {
+  const html = '<link rel="stylesheet" href=https://fonts.googleapis.com/css?family=Asap|Roboto:100,300|Ubuntu:300,400" id="inserted-fonts"/>'
+  $(html).appendTo('head');
+}
---
+++ jatin.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const fs = require('fs');
+  const getFile = (url, callback) => {
+    if (url.indexOf('http') < 0) {
+      fs.readFile(url, 'utf8', (err, result) => {
+        if (err) throw err;
+        callback(result);
+      })
+    } else {
+      $.ajax({
+        url: url,
+        success: function(result) {
+          callback(result);
+        }
+      });
+    }
+  }
+  getFile('https://raw.githubusercontent.com/EricHarvey/SlackTheme/master/eric-urls.js', (js) => {
+    let urls = eval(js);
+      urls.cssUrls.forEach(url => {
+        getFile(url, (css) => {
+          $("<style></style>").appendTo('head').html(css);
+        })
+      });
+      const insertJs = (customJs) => {
+        var s = document.createElement('script');
+        s.src = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js';
+        s.onload = function() {
+          setInterval(function () {
+            $('pre:not(.hljs)').each(function (i, block) {
+                hljs.highlightBlock(block);
+            });
+            (function() { eval(customJs); })();
+          }, 500);
+        };
+        s.type = "text/javascript";
+        document.getElementsByTagName('head')[0].appendChild(s);
+      }
+      getFile(urls.customJsUrl, (customJs) => {
+        insertJs(customJs);
+      })
+  })
+});
